699758b9dd05b932877c0c01f111c79725bb4408
Marco <rubolini.marco@gmail.com>
2020-08-28 16:34:20 +0200

    Creazione migration

---
+++ database/migrations/2020_08_28_140422_create_apartments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->smallInteger('room');
+            $table->smallInteger('bath');
+            $table->smallInteger('square_meters');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('image_url');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
