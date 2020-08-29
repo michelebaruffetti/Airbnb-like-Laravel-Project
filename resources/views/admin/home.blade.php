3533bbd1aff15bca2238035efc53e37d174476c1
maikel abdelmalak <maikel22@hotmail.it>
2020-08-29 12:40:42 +0200

    rotta admin.home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,41 @@
-admin home 
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h1>Elenco Articoli</h1>
+            </div>
+            <div class="col-6 text-right">
+                <a class="btn btn-primary" href="{{route('admin.apartments.create')}}">Aggiungi Post</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                        <th>SLUG</th>
+                        <th class="text-right">AZIONI</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($appartamenti as $appartemento)
+                            <tr>
+                                <td>{{$appartemento->id}}</td>
+                                <td>{{$appartemento->title}}</td>
+                                <td>{{$appartemento->slug}}</td>
+                                <td class="text-right">
+                                    <a class="btn btn-info">Dettagli</a>
+                                    <a class="btn btn-info">Modifica</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+
+@endsection
