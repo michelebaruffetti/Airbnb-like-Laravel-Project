371f53506375cb79c2037d3e2b430e9d13164aae
michele baruffetti <michelebaruffetti@gmail.com>
2020-09-04 13:14:21 +0200

    Proseguimento layout admin show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -22,10 +22,31 @@
 </div>
 <div class="container descrizione">
     <div class="row justify-content-center">
-        <div class="col-9">
-
+        <div class="col-8 testo-descrizione text-left mt-3">
+            <h3>{{$apartment->title}}</h3>
+            <p>{{$apartment->description}}</p>
         </div>
-        <div class="col-3">
+        <div class="col-3 offset-1 caratteristiche text-left mt-3">
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="d-flex justify-content-between w-50">
+                        <i class="fas fa-ruler "></i><span class="badge">area</span>
+                    </div>
+                    <span class="badge ">{{$apartment->square_meters}}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="d-flex justify-content-between w-50">
+                    <i class="fas fa-bed"></i> <span class="badge">stanze</span>
+                    </div>
+                    <span class="badge ">{{$apartment->room}}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="d-flex justify-content-between w-50" >
+                        <i class="fas fa-bath"></i><span class="badge">bagni</span>
+                    </div>
+                    <span class="badge ">{{$apartment->bath}}</span>
+                </li>
+            </ul>
 
         </div>
     </div>
