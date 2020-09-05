038c09b1e479806322c1dc8da5e2fbddc78be936
maikel abdelmalak <maikel22@hotmail.it>
2020-09-05 18:11:44 +0200

    aggiunti campi lastname e birthday al register

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'lastnane', 'birthday', 'email', 'password', 'avatar'
+        'name', 'lastname', 'birthday', 'email', 'password', 'avatar'
     ];
 
     /**
