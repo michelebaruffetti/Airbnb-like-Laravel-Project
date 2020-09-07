6f16004ce0f5e5319e319751dfaa22a909f8a1ec
Marzia Fatarella <marziafatarella@gmail.com>
2020-09-07 16:52:53 +0200

    prova menu mobile

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -24,48 +24,47 @@
 <body>
     <div class="wrapper">
         <nav id="sidebar">
-            <div class="sidebar-header">
-                <p>
+            <div class="sidebar-header d-flex flex-column align-items-center">
+                {{-- mostra avatar solo se presente --}}
+                @if (Auth::user()->avatar)
+                <img src="{{asset('/storage/image/' . Auth::user()->avatar)}}" alt="avatar" width="70">
+                {{-- altrimenti non mostra nulla --}}
+                @endif
+                <p class="mt-2">
                     <span class="icon"><i class="far fa-laugh-wink"></i></span>
                     <span class="title">
                         Ciao {{ Auth::user()->name }}
-
-                        {{-- mostra avatar solo se presente --}}
-                        @if (Auth::user()->avatar)
-                        <img src="{{asset('/storage/image/' . Auth::user()->avatar)}}" alt="avatar" width="70">
-                        {{-- altrimenti non mostra nulla --}}
-                        @endif
                     </span>
                 </p>
             </div>
             <ul class="list-unstyled cambio-active">
             <li class="voce-menu">
                     <a href="{{ route('home') }}">
-                        <span class="icon"><i class="fas fa-home"></i></span>
+                        <span class="icon pr-2"><i class="fas fa-home"></i></span>
                         <span class="title">Visita il sito</span>
                     </a>
                 </li>
                 <li class="voce-menu {{Request::route()->getName() == 'admin.home' ? 'active' : ''}}">
                     <a href="{{ route('admin.home') }}">
-                        <span class="icon"><i class="fas fa-user"></i></span>
+                        <span class="icon pr-2"><i class="fas fa-user"></i></span>
                         <span class="title">Profilo</span>
                     </a>
                 </li>
                 <li class="voce-menu {{Request::route()->getName() == 'admin.apartments.index' ? 'active' : ''}}">
                     <a href="{{ route('admin.apartments.index') }}">
-                        <span class="icon"><i class="fas fa-clipboard-list"></i></span>
+                        <span class="icon pr-2"><i class="fas fa-list"></i></span>
                         <span class="title">Appartamenti</span>
                     </a>
                 </li>
                 <li class="voce-menu {{Request::route()->getName() == 'admin.message' ? 'active' : ''}}">
                     <a href="{{ route('admin.message') }}">
-                        <span class="icon"><i class="fas fa-envelope"></i></span>
+                        <span class="icon pr-2"><i class="fas fa-envelope"></i></span>
                         <span class="title">Messaggi</span>
                     </a>
                 </li>
                 <li class="voce-menu">
                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
+                        <span class="icon pr-2"><i class="fas fa-sign-out-alt"></i></span>
                         <span class="title">Logout</span>
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -73,13 +72,54 @@
                     </form>
                 </li>
             </ul>
-
-            {{-- <div class="toggle">
-
-            </div> --}}
         </nav>
         <div id="content">
             @yield('content')
+        </div>
+        <div id="toggle" class="p-4 d-md-none">
+            <a href="#">
+                <i class="fas fa-3x fa-bars"></i>
+            </a>
+            <div class="hamburger-menu">
+                <ul class="">
+                    <li class="voce-menu">
+                            <a href="{{ route('home') }}">
+                                <span class="icon pr-2"><i class="fas fa-home"></i></span>
+                                <span class="title">Visita il sito</span>
+                            </a>
+                        </li>
+                        <li class="voce-menu {{Request::route()->getName() == 'admin.home' ? 'active' : ''}}">
+                            <a href="{{ route('admin.home') }}">
+                                <span class="icon pr-2"><i class="fas fa-user"></i></span>
+                                <span class="title">Profilo</span>
+                            </a>
+                        </li>
+                        <li class="voce-menu {{Request::route()->getName() == 'admin.apartments.index' ? 'active' : ''}}">
+                            <a href="{{ route('admin.apartments.index') }}">
+                                <span class="icon pr-2"><i class="fas fa-list"></i></span>
+                                <span class="title">Appartamenti</span>
+                            </a>
+                        </li>
+                        <li class="voce-menu {{Request::route()->getName() == 'admin.message' ? 'active' : ''}}">
+                            <a href="{{ route('admin.message') }}">
+                                <span class="icon pr-2"><i class="fas fa-envelope"></i></span>
+                                <span class="title">Messaggi</span>
+                            </a>
+                        </li>
+                        <li class="voce-menu">
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <span class="icon pr-2"><i class="fas fa-sign-out-alt"></i></span>
+                                <span class="title">Logout</span>
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                <a href="#" class="close">
+                    <i class="fas fa-times"></i>
+                </a>
+            </div>
         </div>
     </div>
     @yield('script')
