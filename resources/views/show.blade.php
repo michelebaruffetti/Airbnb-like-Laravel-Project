7ad645c08b30f112e6af94a3b5bb380bbbe903fc
michele baruffetti <michelebaruffetti@gmail.com>
2020-09-09 13:02:17 +0200

    Show Guest Work in Progress

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -17,42 +17,50 @@
 {{-- DETTAGLI APPARTAMENTO GUEST --}}
 
 {{-- sezione immagine --}}
-<div class="container-fluid immagine mt-3">
-
-    <div class="row">
-        <div class="col-9 text-center ">
+{{-- <div class="container-fluid immagine mt-3">
+    <div class="row  ">
+        <div class="col-12 d-flex justify-content-center">
             @if ($apartment->image_url)
-                <img class="img-fluid rounded img-appartamento" src="{{asset('storage/' . $apartment->image_url)}}" alt="foto appartamento">
+                <img class="img-fluid rounded img-appartamento" src="{{asset('storage/' . $apartment->image_url)}}"  alt="foto appartamento">
             @else
-                <img class="img-fluid rounded img-appartamento" src="https://image.freepik.com/vettori-gratuito/banner-di-twitch-offline-carino-con-gatto_23-2148588262.jpg" alt="foto gatto">
+                <img class="img-fluid rounded img-appartamento" src="https://image.freepik.com/vettori-gratuito/banner-di-twitch-offline-carino-con-gatto_23-2148588262.jpg"  alt="foto gatto">
             @endif
 
         </div>
     </div>
-</div>
+</div> --}}
 
 {{-- sezione testuale --}}
 <div class="container descrizione">
     <div class="row justify-content-center mb-3">
 
+
         {{-- sezione sinistra titolo + descrizione + promozioni --}}
-        <div class="col-9 testo-descrizione text-left mt-3">
-            <h3>{{$apartment->title}}</h3>
+        <div class="col-12 col-lg-9 testo-descrizione text-left pt-4">
+            <div class="immagine text-center">
+                @if ($apartment->image_url)
+                    <img class="img-fluid rounded img-appartamento" src="{{asset('storage/' . $apartment->image_url)}}"  alt="foto appartamento">
+                @else
+                    <img class="img-fluid rounded img-appartamento" src="https://image.freepik.com/vettori-gratuito/banner-di-twitch-offline-carino-con-gatto_23-2148588262.jpg"  alt="foto gatto">
+                @endif
+            </div>
+
+            <h3 class="pt-4">{{$apartment->title}}</h3>
             <div id="descrizione_appartamento">
-                <p>{{$apartment->description}}</p>
+                <p class="text-justify">{{$apartment->description}}</p>
             </div>
 
             {{-- SEZIONE MAPPA APPARTAMENTO --}}
-            <div class="container promozioni mb-4">
+            <div class="container promozioni py-3">
                 <div class="row">
-                    <div class="col-10 offset-1">
-                        <h3 class="text-center">Localizzazione appartamento</h3>
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h3 class="h4 text-center">Localizzazione appartamento</h3>
                     </div>
                 </div>
 
 
                 <div class="row  mt-2">
-                    <div class="col-10 offset-1">
+                    <div class="col-12 col-lg-10 offset-lg-1">
                         <div class="promozione rounded border border-color-grey py-2 mt-1 mb-1 text-center bg-info">
                             <div id="map"></div>
                             <script>
@@ -74,31 +82,50 @@
         </div>
 
         {{-- sezione destra dettagli + servizi + statistiche --}}
-        <div class="col-3 caratteristiche text-left mt-3">
+        <div class="col-12 col-lg-3 caratteristiche text-left py-4">
             <div class="dettagli">
                 <h6 class="text-center ">Dettagli</h6>
                 <ul class="list-group">
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <div class="d-flex justify-content-between w-50">
-                            <i class="fas fa-ruler "></i><span class="badge">area</span>
+                    <li class="list-group-item d-flex text-center align-items-center">
+                        <div class="w-25">
+                            <i class="fas fa-ruler "></i>
                         </div>
-                        <span class="badge ">{{$apartment->square_meters}}</span>
+                        <div class="w-50">
+                            <span class="badge">area</span>
+                        </div>
+                        <div class="w-25">
+                            <span class="badge">{{$apartment->square_meters}}</span>
+                        </div>
 
                     </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <div class="d-flex justify-content-between w-50">
-                            <i class="fas fa-bed"></i> <span class="badge">stanze</span>
+                    <li class="list-group-item d-flex text-center align-items-center">
+                        <div class="w-25">
+                            <i class="fas fa-bed"></i>
                         </div>
-                        <span class="badge ">{{$apartment->room}}</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <div class="d-flex justify-content-between w-50" >
-                            <i class="fas fa-bath"></i><span class="badge">bagni</span>
+                        <div class="w-50">
+                            <span class="badge">stanze</span>
                         </div>
-                        <span class="badge ">{{$apartment->bath}}</span>
+                        <div class="w-25">
+                            <span class="badge">{{$apartment->room}}</span>
+                        </div>
+
                     </li>
+                    <li class="list-group-item d-flex text-center align-items-center">
+                        <div class="w-25">
+                            <i class="fas fa-bath"></i>
+                        </div>
+                        <div class="w-50">
+                            <span class="badge">bagni</span>
+                        </div>
+                        <div class="w-25">
+                            <span class="badge">{{$apartment->bath}}</span>
+                        </div>
+
+                    </li>
+
                 </ul>
             </div>
+
 
             <div class="servizi">
                 <h6 class="text-center mt-3">Servizi Aggiuntivi</h6>
@@ -106,14 +133,28 @@
                     <ul class="list-group servizi">
                         @forelse($apartment->services as $service)
                                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <i class="fas fa-check-circle"></i><span>
-                                <span class="badge ">{{$service->description}}</span>
+                                    <div class="w-25 text-center">
+                                        <i class="fas fa-check-circle"></i><span>
+                                    </div>
+                                    <div class="w-50">
+
+                                    </div>
+                                    <div class="w-25 text-center">
+                                        <span class="badge ">{{$service->description}}</span>
+                                    </div>
                                 </li>
                             </ul>
                         @empty
-                                <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <i class="far fa-check-circle"></i><span>
-                                <span class="badge ">Nessun servizio aggiuntivo</span>
+                                <li class="list-group-item d-flex text-center align-items-center">
+                                    <div class="w-25">
+                                        <i class="far fa-check-circle"></i><span>
+                                    </div>
+                                    <div class="w-50">
+
+                                    </div>
+                                    <div class="w-25">
+                                        <span class="badge ">Nessuno</span>
+                                    </div>
                                 </li>
                         @endforelse
                     </ul>
