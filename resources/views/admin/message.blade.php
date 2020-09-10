7a6f52c62e0da4feecf20994fc8bcd5d3ab5a2cf
michele baruffetti <michelebaruffetti@gmail.com>
2020-09-10 11:50:27 +0200

    Visualizzazione messaggi ok.
    Dibattito estetico aperto. :D

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -4,32 +4,36 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>I Tuoi Messaggi</h1>
+                <h1 class="h6">I Tuoi Messaggi:</h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-12">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">Apartment ID</th>
-                      <th scope="col">Client Name</th>
-                      <th scope="col">Client Email</th>
-                      <th scope="col">Mesasge</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      @foreach ($messages as $message)
-                            <th scope="row">{{$message->apartment_id}}</th>
-                            <td>{{$message->name}} {{$message->lastname}}</td>
-                            <td>{{$message->email}}</td>
-                            <td>{{$message->text}}</td>
-                          </tr>
-                      @endforeach
+            @forelse ($messages as $message)
+                <div class="col-12 border rounded text-center my-2 p-3">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-5 d-flex flex-column justify-content-center">
+                                <h3 class="h5">ID dell'appartamento: {{$message->apartment_id}}</h3>
+                                <p class="mb-0"><strong>Mittente:</strong> {{$message->name}} {{$message->lastname}}</p>
+                                <p class="mb-0"><strong>Email:</strong> {{$message->email}}</p>
+                            </div>
+                            <div class="col-7 d-flex flex-column justify-content-center">
+                                <p><strong>Richiesta:</strong> {{$message->text}}</p>
+                            </div>
 
-                  </tbody>
-                </table>
-            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            @empty
+                <div class="col-12 border rounded text-center ">
+                    <p class="p-2">Non hai messaggi da visualizzare.</p>
+
+                </div>
+            @endforelse
         </div>
     </div>
 @endsection
