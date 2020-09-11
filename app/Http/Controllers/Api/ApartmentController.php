3b21262652bc4b6a95308b1b5fe74a56d8600701
maikel abdelmalak <maikel22@hotmail.it>
2020-09-11 19:08:52 +0200

    query raggio-distanza nell'api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -9,23 +9,26 @@ use App\Apartment;
 
 class ApartmentController extends Controller
 {
+    public function all(Request $request)
+    {
 
-    public function all(Request $request){
+        $latitude= 45.4335;
+        $longitude= 9.1159;
+        $distance= 20000000000;
 
-        $apartments = Apartment::all()->where('status', '1');
-
-        return response()->json($apartments);
-    }
-
-    public function sponsored(){
-        $oggi = date("Y/m/d");
-        $apartments = DB::table('apartments')
-            ->join('apartment_sponsor', 'apartments.id', '=', 'apartment_sponsor.apartment_id')
-            ->where([
-                ['status', '=', '1'],
-                ['end_date', '>', $oggi],
-                ])
+        $results = Apartment::select(DB::raw('*, ( 6367 * acos( cos( radians('.$latitude.') ) * cos( radians( latitude ) ) * cos(
+            radians( longitude ) - radians('.$longitude.') ) + sin( radians('.$latitude.') ) * sin( radians( latitude ) ) ) ) AS
+            distance'))
+            ->having('distance', '<', $distance)
+            ->orderBy('distance')
+            ->where('status', '1')
+            // ->rightJoin('apartment_service', 'apartments.id', '=', 'apartment_service.apartment_id')
+            // ->rightJoin('services', 'apartment_service.service_id', '=', 'services.id')
             ->get();
-            return response()->json($apartments);
+        $results=$results->services;
+        return response()->json($results);
     }
+
+
+
 }
