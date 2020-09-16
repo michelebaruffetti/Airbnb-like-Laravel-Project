92f7abe533012cb03d86a0baa7446920b9c7f965
michele baruffetti <michelebaruffetti@gmail.com>
2020-09-16 17:47:43 +0200

    Rifinito il Form di Ricerca

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,76 +2,33 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <h1>Questa è la home generale</h1>
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <form action="{{Route('search')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group row">
+                        <div class="col-10">
+                            <label for="form-address" class="col-form-label label-ricerca">Ricerca il tuo appartamento: </label>
+                            <input class="testo-ricerca" type="text"  id="form-address" name="address" value="{{old('address')}}" placeholder="In quale città vuoi andare?">
+                        </div>
+                        <div class="col-2 d-flex">
+                            <button id="btn-cerca" type="submit" class="btn ">Cerca</button>
+                        </div>
+                    </div>
+                        {{-- form nascosto --}}
+                        <div class="form-group">
+                            <input type="hidden" name="latitude" class="form-control" id="latitude" value="{{old('latitude')}}">
+                            <input type="hidden" name="longitude" class="form-control" id="longitude" value="{{old('longitude')}}">
+                            <input type="hidden" name="range" class="form-control" id="range" value="20">
+                        </div>
+                </form>
             </div>
-        </div>
-        <form class="" action="{{Route('search')}}" method="post" enctype="multipart/form-data">
-            @csrf
-        <div class="row">
-            <div class="col-8">
-                <div class="form-group">
-                    <label for="form-address">Indirizzo</label>
-                    <input type="text" class="form-control" id="form-address" name="address" value="{{old('address')}}" placeholder="Inserisci il tuo indirizzo">
-                </div>
-                <div class="form-group">
-                    <input type="hidden" name="latitude" class="form-control" id="latitude" value="{{old('latitude')}}">
-                    <input type="hidden" name="longitude" class="form-control" id="longitude" value="{{old('longitude')}}">
-                    <input type="hidden" name="range" class="form-control" id="range" value="20">
-                </div>
-            </div>
-            <div class="col-2">
-                <button type="submit" class="btn btn-primary">Cerca</button>
-            </div>
+
         </div>
 
-        </form>
     </div>
 
-
-    <section class="carousel mb-3">
-        <div class="container">
-            <div class="row justify-items-center">
-                <div class="col-12 col-md-8 offset-md-2">
-                    <div id="carousel-sponsor" class="carousel slide" data-ride="carousel">
-
-                        <!-- immagini carosello -->
-                        <div class="carousel-inner text-center">
-                            @foreach($apartments as $apartment)
-                              <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-                                  @if ($apartment->image_url)
-                                      <img class="rounded img-fluid " src="{{asset('storage/' . $apartment->image_url)}}" alt="foto-appartamento">
-                                  @else
-                                      {{-- <img class="rounded img-fluid" src="{{asset('storage/not-found/not-found.png')}}" alt="foto-appartamento"> --}}
-                                      <img class="img-fluid rounded" src="https://image.freepik.com/vettori-gratuito/banner-di-twitch-offline-carino-con-gatto_23-2148588262.jpg" alt="foto gatto">
-                                  @endif
-                                     <div class="carousel-caption d-none d-md-block">
-                                        <h3>{{$apartment->title}}</h3>
-                                        <p>{{$apartment->description }}</p>
-                                     </div>
-                              </div>
-                            @endforeach
-                        </div>
-
-                        <!-- controlli carosello -->
-                        <a class="carousel-control-prev" href="#carousel-sponsor" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carousel-sponsor" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-    </section>
-
-    {{-- <div class="container lista-cards">
+    <div class="container lista-cards">
         <div class="row mb-2">
             <h1>SPONSORIZZATI</h1>
             @forelse ($apartments as $apartment)
@@ -79,7 +36,7 @@
                 <div class="col-4  immagine d-flex align-items-center">
                     @if ($apartment->image_url)
                         <img class="rounded img-fluid " src="{{asset('storage/' . $apartment->image_url)}}" alt="foto-appartamento">
-                    @else --}}
+                    @else
                         {{-- <img class="rounded img-fluid" src="{{asset('storage/not-found/not-found.png')}}" alt="foto-appartamento"> --}}
                         <img class="img-fluid rounded" src="https://image.freepik.com/vettori-gratuito/banner-di-twitch-offline-carino-con-gatto_23-2148588262.jpg" alt="foto gatto">
                     @endif
@@ -105,8 +62,8 @@
             <p>non ci sono Appartamenti sponsorizzati</p>
             @endforelse
         </div>
-        @endforelse
-    </div> --}}
+
+    </div>
 
 
 @endsection
