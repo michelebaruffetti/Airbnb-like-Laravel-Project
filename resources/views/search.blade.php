fc1723b26b7a874c45140bb2dbfbb14635414f1e
michele baruffetti <michelebaruffetti@gmail.com>
2020-09-18 14:02:15 +0200

    Form Ricerca Search in corso...

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,10 +3,10 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-8">
+        <div class="col-12 col-sm-8" >
             <div class="form-group">
                 <label for="form-address">Indirizzo</label>
-                <input type="text" class="form-control" id="form-address" name="address" value="{{old('address', $ricerca['address'])}}" placeholder="Inserisci il tuo indirizzo">
+                <input type="text" class="form-control testo-ricerca" id="form-address" name="address" value="{{old('address', $ricerca['address'])}}" placeholder="Inserisci il tuo indirizzo">
             </div>
             <div class="form-group">
                 <input type="hidden" name="latitude" class="form-control" id="latitude" value="{{old('latitude', $ricerca['latitude'])}}">
@@ -15,13 +15,14 @@
         </div>
         <div class="form-group col-4">
             <label for="range">Raggio</label>
-            <select class="form-control" name="range" id="range">
+            <select class="form-control testo-ricerca" name="range" id="range">
                 <option {{$ricerca['range'] == '20' ? 'selected' : '' }} value="20">20</option>
                 <option {{$ricerca['range'] == '50' ? 'selected' : '' }} value="50">50</option>
                 <option {{$ricerca['range'] == '100' ? 'selected' : '' }} value="100">100</option>
             </select>
         </div>
     </div>
+
     <div class="row">
         <div class="form-group col-10 d-flex">
             Servizi:
