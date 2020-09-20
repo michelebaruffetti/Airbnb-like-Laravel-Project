1268ca8deb93c0ddb7ee00c29c6ddc28a031db96
Marco <rubolini.marco@gmail.com>
2020-09-20 18:09:22 +0200

    Modifica grafico

--- public/js/grafici.js
+++ public/js/grafici.js
@@ -48375,12 +48375,18 @@ $(document).ready(function () {
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Messaggi', 'Visualizzazioni'],
+      labels: ['Messaggi e visualizzazioni ricevute'],
       datasets: [{
-        label: ['Messaggi', 'Visualizzazioni'],
-        data: [messaggi, visualizzazioni],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+        label: ['Messaggi'],
+        data: [messaggi],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)'],
+        borderWidth: 1
+      }, {
+        label: ['Visualizzazioni'],
+        data: [visualizzazioni],
+        backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgba(54, 162, 235, 1)'],
         borderWidth: 1
       }]
     },
--- public/js/search.js
+++ public/js/search.js
@@ -16098,14 +16098,18 @@ $(document).ready(function () {
   $('#ricerca').click(function () {
     $('#contenitore-appartamenti').empty();
     ricerca();
-  });
+  }); //al change delle select recupero i dati negli input nascosti e faccio la chiamata ajax
+
   $('.filtri-select').change(function () {
     $('#contenitore-appartamenti').empty();
     ricerca();
-  });
-  var servizi_selezionati = [];
+  }); // Genero un array dei servizi selezionati dall'utente vuoto
+
+  var servizi_selezionati = []; // al click dei servizi 
+
   $('.servizi').on('click', function () {
-    var servizio_selezionato = $(this).val();
+    // recupero l'id del servizio selezionato
+    var servizio_selezionato = $(this).val(); // se non è incluso nell'array servizi selezionati lo pusho altrimenti lo tolgo
 
     if (servizi_selezionati.includes(servizio_selezionato)) {
       var indice = servizi_selezionati.indexOf(servizio_selezionato);
@@ -16114,13 +16118,11 @@ $(document).ready(function () {
       servizi_selezionati.push(servizio_selezionato);
     }
 
-    console.log('servizio-selezionato: ' + servizio_selezionato);
-    console.log('Array servizi selezionati: ' + servizi_selezionati);
     ricerca();
   });
 
   function ricerca() {
-    // $('#contenitore-appartamenti').empty();
+    // recupero tutti i dati dagli input
     var lat = $('#latitude').val();
     var lng = $('#longitude').val();
     var rag = $('#range').val();
@@ -16143,10 +16145,12 @@ $(document).ready(function () {
       success: function success(data) {
         $('#contenitore-appartamenti').empty();
         var source = $("#template-apartment").html();
-        var template = Handlebars.compile(source); // var sentinella = 0;
+        var template = Handlebars.compile(source); // ciclo i dati della risposta
 
         for (var i = 0; i < data.response.length; i++) {
-          var servizi_appartamento = [];
+          // creo un array per i servizi di ogni appartamento
+          var servizi_appartamento = []; // se la descrizione è lunga la tronco
+
           var descr = data.response[i].description;
 
           if (descr.length > 450) {
@@ -16160,47 +16164,42 @@ $(document).ready(function () {
             description: descr,
             services: data.response[i].services,
             id_apartment: data.response[i].id
-          };
-          var sentinella = 0;
+          }; // creo una variabile sentinella
+
+          var sentinella = 0; // ciclo i servizi di ogni appartamento
 
           for (var j = 0; j < data.response[i].services.length; j++) {
-            // var servizio = data.response[i].services[j];
+            // li pusho all'interno dell'array creato in precedenza
             servizi_appartamento.push(data.response[i].services[j].id);
           }
 
-          console.log(servizi_appartamento);
-          var html = template(context);
+          var html = template(context); // se i servizi selezionati sono 0 disegno la card 
 
           if (servizi_selezionati.length == 0) {
-            $('#contenitore-appartamenti').append(html);
+            $('#contenitore-appartamenti').append(html); // altrimenti ciclo i servizi selezionati dall'utente
           } else {
             for (var y = 0; y < servizi_selezionati.length; y++) {
-              console.log(servizi_selezionati[y]);
-              var pippo = Number(servizi_selezionati[y]);
+              // transformo in numeri i valori
+              var servizi_selezionati_utente = Number(servizi_selezionati[y]); // se i servizi selezionati sono inclusi nell'array dei servizi dell'appartamento
 
-              if (servizi_appartamento.includes(pippo)) {
-                // $('#contenitore-appartamenti').empty();
+              if (servizi_appartamento.includes(servizi_selezionati_utente)) {
+                // aumento la sentinella di 1
                 sentinella = sentinella + 1;
-                console.log(sentinella); // $('#contenitore-appartamenti').append(html);
               }
 
               ;
-            }
+            } // se la sentinella è uguale al numero dei servizi selezionati dall'utente disegno la card
+
 
             if (sentinella == servizi_selezionati.length) {
               $('#contenitore-appartamenti').append(html);
-            } // console.log(sentinella);
-            // if (sentinella == servizi_selezionati.length){
-            //     $('#contenitore-appartamenti').append(html);
-            // }
-
+            }
           }
 
-          ; // $('#contenitore-appartamenti').append(html);
+          ;
         }
 
         ;
-        console.log(data);
       }
     });
   }
